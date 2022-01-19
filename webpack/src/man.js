7871c87db1d4f32960e05cd7851ba421c6b6aa8d
xumeijie <xcdh560@foxmail.com>
2022-01-19 17:24:47 +0800

    loader-default

--- webpack/src/man.js
+++ webpack/src/man.js
@@ -1,4 +1,8 @@
 // const {add} = require('./muth.js');
-import {add} from './muth.js';
+import {add} from './js/muth.js';
+// 依赖css文件
+import css from './css/norme.css';
+// require('./css/norme');
+
 
 console.log(add(10, 20));
